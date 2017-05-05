2cb2be85350d7e06af0e67994c3f567fd175b138
Porag Chowdhury <poragbuet@gmail.com>
2017-05-04 23:13:23 -0600

    new results added

--- src/LogParser.java
+++ src/LogParser.java
@@ -17,9 +17,114 @@ public class LogParser {
 	 * @param args
 	 */
 	public static void main(String[] args) {
+		hourAheadErrorParser();
+	}
+	
+	public static void hourAheadErrorParser(){
+		double [][] errorValue = new double [7][10];
+		double [][] errorCounter = new double [7][10];
+		String fileName = "high_demand_mcts_HA_prediction_error.csv";
+		File gFile = new File(fileName);
+        if(!gFile.exists()){
+            System.out.println("Load file doesn't exist");
+        	return;
+        }
+        int mctsITER = 0;
+        CSVParser parser;
+		try {
+			boolean print = false;
+			parser = CSVParser.parse(gFile, StandardCharsets.US_ASCII, CSVFormat.DEFAULT);
+			
+			FileWriter fwOutput2 = new FileWriter(fileName+".50KScattered.csv", true);
+			PrintWriter pwOutput2 = new PrintWriter(new BufferedWriter(fwOutput2));
+			pwOutput2.println("HA,Error,");
+			
+			
+
+			for (CSVRecord csvRecord : parser) {
+	            Iterator<String> itr = csvRecord.iterator();
+	            // Time Stamp	
+	            String strMCTS = itr.next();
+	            if(strMCTS.equalsIgnoreCase("MCTS_SIM")){
+	            	// Skip
+	            }
+	            else{
+	            	// Check MCTS iteration
+	            	String strDay = itr.next();
+	            	String strHour = itr.next();
+	            	String strHA = itr.next();
+	            	int HA = Integer.parseInt(strHA);
+	            	String strError = itr.next();
+	            	int mcts;
+	            	if(strMCTS.equalsIgnoreCase("100"))
+	            		mcts = 0;
+	            	else if(strMCTS.equalsIgnoreCase("1000"))
+	            		mcts = 1;
+	            	else if(strMCTS.equalsIgnoreCase("5000"))
+	            		mcts = 2;
+            		else if(strMCTS.equalsIgnoreCase("10000"))
+	            		mcts = 3;
+        			else if(strMCTS.equalsIgnoreCase("15000"))
+	            		mcts = 4;
+    				else if(strMCTS.equalsIgnoreCase("25000"))
+	            		mcts = 5;
+    				else if(strMCTS.equalsIgnoreCase("50000")){
+	            		mcts = 6;
+	            		print = true;
+    				}
+    				else{
+    					System.out.println("Missmatch!");
+    					return;
+    				}
+	            	
+	            	if(print){
+	            		pwOutput2.println(strHA + "," + strError+",");
+	            		print = false;
+	            	}
+	            	
+            		errorValue[mcts][HA] += Double.parseDouble(strError);
+            		errorCounter[mcts][HA] += 1;
+	            
+	            }
+			}
+			
+			parser.close();
+			pwOutput2.close();
+			fwOutput2.close();
+			
+			for(int j = 0; j<7; j++){
+				for(int val =0;val<10;val++){
+					errorValue[j][val] /= errorCounter[j][val];
+				}
+			}
+			
+			
+			FileWriter fwOutput = new FileWriter(fileName+".AvgParsed.csv", true);
+			PrintWriter pwOutput = new PrintWriter(new BufferedWriter(fwOutput));
+						
+			pwOutput.println("MCTS,0,1,2,3,4,5,6,7,8,9");
+			for(int mcts = 0; mcts < 7; mcts++){
+				pwOutput.print(mcts+",");
+				for(int agent = 0; agent < 10; agent++){
+					pwOutput.print(errorValue[mcts][agent]+",");
+				}
+				pwOutput.println();
+			}
+			
+			pwOutput.close();
+			fwOutput.close();
+			
+			
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	public void costValueParser(){
 		
 		double [][][] arrAgents = new double[5][7][10];
-		String fileName = "equal_demand_Results_Price.csv";
+		String fileName = "low_demand_Results_Price.csv";
 		File gFile = new File(fileName);
         if(!gFile.exists()){
             System.out.println("Load file doesn't exist");
